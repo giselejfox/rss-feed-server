5a2dd991fd0c0d2a6da07e95d5544740e4371640
Gisele Fox <72312641+giselejfox@users.noreply.github.com>
2024-12-08 21:56:47 -0800

    test new forebase config key

--- server.js
+++ server.js
@@ -5,15 +5,21 @@ import admin from 'firebase-admin';
 import dotenv from 'dotenv';
 dotenv.config();
 
+
+
 // Initialize Firebase Admin SDK
 if (!admin.apps.length) {
+    const firebase_private_key_b64 = Buffer.from(process.env.FIREBASE_PRIVATE_KEY_BASE64, 'base64');
+    const firebase_private_key = firebase_private_key_b64.toString('utf8');
+    console.log("firebase private key " + firebase_private_key)
     const serviceAccount = {
         type: "service_account",
         project_id: process.env.FIREBASE_PROJECT_ID,
         private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-        private_key: process.env.FIREBASE_PRIVATE_KEY
-            .replace(/\\n/g, '\n') // Fix newlines
-            .replace(/_/g, ' '), // Replace underscores with spaces
+        // private_key: process.env.FIREBASE_PRIVATE_KEY
+        //     .replace(/\\n/g, '\n') // Fix newlines
+        //     .replace(/_/g, ' '), // Replace underscores with spaces
+        private_key: firebase_private_key,
         client_email: process.env.FIREBASE_CLIENT_EMAIL,
         client_id: process.env.FIREBASE_CLIENT_ID,
         auth_uri: "https://accounts.google.com/o/oauth2/auth",
