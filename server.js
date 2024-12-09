712de2affef36eb57405ae5f60f0f79c225d4f66
Gisele Fox <72312641+giselejfox@users.noreply.github.com>
2024-12-08 22:19:45 -0800

    test private key process

--- server.js
+++ server.js
@@ -49,13 +49,11 @@ app.get("/", function(req, res){
 })
 
 app.get("/other", function(req, res){
-    const firebase_private_key_b64 = Buffer.from(process.env.FIREBASE_PRIVATE_KEY_BASE64, 'base64');
-    const firebase_private_key = firebase_private_key_b64.toString('utf8');
     res.json({
         message: "working test test",
         data: process.env.FIREBASE_PRIVATE_KEY
-            .replace(/\\n/g, '\n') //` Fix newlines
-            .replace(/_/g, ' '), // Replace underscores with spaces`
+            // .replace(/\\n/g, '\n') //` Fix newlines
+            // .replace(/_/g, ' '), // Replace underscores with spaces`
     })
 })
 
