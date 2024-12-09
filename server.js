78a8a14d900ecd39d83820bf83979cbd143ee121
Gisele Fox <72312641+giselejfox@users.noreply.github.com>
2024-12-08 22:24:05 -0800

    test private key process

--- server.js
+++ server.js
@@ -7,34 +7,30 @@ dotenv.config();
 
 
 // Initialize Firebase Admin SDK
-// if (!admin.apps.length) {
-//     const firebase_private_key_b64 = Buffer.from(process.env.FIREBASE_PRIVATE_KEY_BASE64, 'base64');
-//     const firebase_private_key = firebase_private_key_b64.toString('utf8');
-//     console.log("firebase private key " + firebase_private_key)
-//     const serviceAccount = {
-//         type: "service_account",
-//         project_id: process.env.FIREBASE_PROJECT_ID,
-//         private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-//         // private_key: process.env.FIREBASE_PRIVATE_KEY
-//         //     .replace(/\\n/g, '\n') // Fix newlines
-//         //     .replace(/_/g, ' '), // Replace underscores with spaces
-//         private_key: firebase_private_key,
-//         client_email: process.env.FIREBASE_CLIENT_EMAIL,
-//         client_id: process.env.FIREBASE_CLIENT_ID,
-//         auth_uri: "https://accounts.google.com/o/oauth2/auth",
-//         token_uri: "https://oauth2.googleapis.com/token",
-//         auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-//         client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
-//     };
-//     admin.initializeApp({
-//         credential: admin.credential.cert(serviceAccount),
-//         databaseURL: process.env.FIREBASE_DATABASE_URL,
-//     });
-// } else {
-//     admin.app(); // Use the default app if already initialized
-// }
+if (!admin.apps.length) {
+    const serviceAccount = {
+        type: "service_account",
+        project_id: process.env.FIREBASE_PROJECT_ID,
+        private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+        private_key: process.env.FIREBASE_PRIVATE_KEY
+            .replace(/\\n/g, '\n') // Fix newlines
+            .replace(/_/g, ' '), // Replace underscores with spaces
+        client_email: process.env.FIREBASE_CLIENT_EMAIL,
+        client_id: process.env.FIREBASE_CLIENT_ID,
+        auth_uri: "https://accounts.google.com/o/oauth2/auth",
+        token_uri: "https://oauth2.googleapis.com/token",
+        auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+        client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+    };
+    admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+        databaseURL: process.env.FIREBASE_DATABASE_URL,
+    });
+} else {
+    admin.app(); // Use the default app if already initialized
+}
 
-// const db = admin.database();
+const db = admin.database();
 
 const app = express()
 app.use(bodyParser.json());
